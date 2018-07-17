f629c5a6d3be4feb60ace43becdaa4593e67368f
iprada <inigo.luengo@bio.ku.dk>
2018-07-17 16:29:42 +0200

    finished the pipeline and begin cleaning the temp files

--- circrna_sbatch.sh
+++ circrna_sbatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition normal
-#SBATCH --mem 32G
-#SBATCH -c 6
-#SBATCH -t 00:10:00
+#SBATCH --mem 64G
+#SBATCH -c 16
+#SBATCH -t 01:00:00
 #SBATCH -o slurm_out
 
 
@@ -10,4 +10,4 @@
 export PATH="/home/iprada/bin/anaconda3/bin:$PATH"
 #script
 
-snakemake --snakefile circ_rna_snakemake.snk --dir /home/iprada/faststorage/projects/circrna_snakemake/working_directory/ --cores 4
+snakemake --snakefile circ_rna_snakemake.snk --latency-wait 30 --dir /home/iprada/faststorage/projects/circrna_snakemake/working_directory/ --cores 16
